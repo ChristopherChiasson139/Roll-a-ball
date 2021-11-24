ec4c4e1225682b6f9dfc06f369a59bac6b5b58ab
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2021-11-24 12:39:46 -0400

    Fixed platforms,brighter glow,added and fix pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 46)
+        if (count >= 50)
         {
             winTextObject.SetActive(true);
         }
