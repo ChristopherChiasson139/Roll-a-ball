6dcf54afc93dca3833f6df603377ea7bfe0e8307
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2021-11-26 11:56:08 -0400

    ???

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+
+            if (OneTimeTrigger == true)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+
+
+
+}
